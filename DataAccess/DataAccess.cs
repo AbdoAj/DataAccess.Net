b1c001f688c7702951c37e94990e445867c16899
AbdoAj <abdoo.aj93@gmail.com>
2022-11-07 21:29:22 +0200

    create project

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,306 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessDotNet
+{
+    public class DataAccess: IDataAccess
+    {
+        private readonly IConfiguration _Config;
+        public string ConnectionString;
+
+        public SqlConnection sqlConnection;
+        private DbTransaction sqlTransaction;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _Config = configuration;
+            ConnectionString = _Config.GetConnectionString("EsemsSchoolsDB");
+            sqlConnection = new SqlConnection(ConnectionString);
+        }
+
+        #region Sql Methods 
+        #region Select
+        public async Task<T> SqlSelectAsync<T>(string query) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Initialize<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw; }
+
+        }
+        public async Task<T> SqlSelectAsync<T>(string query, SqlParameter sqlParameters) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+
+                    cmd.Parameters.Add(sqlParameters);
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Initialize<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw; }
+
+        }
+        public async Task<T> SqlSelectAsync<T>(string query, SqlParameter[] sqlParameters) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+
+                    cmd.Parameters.AddRange(sqlParameters);
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await Initialize<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw; }
+
+        }
+        #endregion
+
+        #region SelectList
+        public async Task<List<T>> SqlSelectListAsync<T>(string query) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await InitializeList<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw; }
+
+        }
+        public async Task<List<T>> SqlSelectListAsync<T>(string query, SqlParameter[] sqlParameters) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+
+                    cmd.Parameters.AddRange(sqlParameters);
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await InitializeList<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw ; }
+
+        }
+        public async Task<List<T>> SqlSelectListAsync<T>(string query, SqlParameter sqlParameter) where T : new()
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+
+                    cmd.Parameters.Add(sqlParameter);
+                    cmd.CommandTimeout = 0;
+                    using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                    {
+                        return await InitializeList<T>(reader);
+
+                    }
+                }
+            }
+            catch (Exception) { throw ; }
+
+        }
+        #endregion
+
+        public async Task SqlExecuteStoredProcedureAsync(SqlCommand cmd)
+        {
+
+            try
+            {
+                cmd.Transaction = (SqlTransaction)sqlTransaction;
+                cmd.Connection = sqlConnection;
+                cmd.CommandTimeout = 0;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (SqlException) { throw; }
+            catch (Exception) { throw; }
+
+        }
+        public async Task SqlExcuteQueryAsync(string query)
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+                    cmd.CommandTimeout = 0;
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (SqlException) { throw; }
+            catch (Exception) { throw; }
+
+        }
+        public async Task SqlExcuteQueryAsync(string query, SqlParameter[] sqlParameters)
+        {
+
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                {
+                    cmd.Transaction = sqlTransaction as SqlTransaction;
+                    cmd.CommandTimeout = 0;
+                    cmd.Parameters.AddRange(sqlParameters);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+            catch (SqlException) { throw ; }
+            catch (Exception) { throw; }
+
+        }
+        #endregion
+
+
+        #region SqlConnection & DbTransaction
+        private async Task OpenConnectionAsync()
+        {
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                    await sqlConnection.OpenAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private async Task CloseConnectionAsync()
+        {
+            await sqlConnection.CloseAsync();
+        }
+        public async Task BeginTransactionAsync()
+        {
+            sqlTransaction = await sqlConnection.BeginTransactionAsync();
+        }
+        public async Task CommitTransactionAsync()
+        {
+            await sqlTransaction.CommitAsync();
+        }
+        public async Task RollbackTransactionAsync()
+        {
+            if (sqlTransaction != null && sqlTransaction.Connection != null)
+                await sqlTransaction.RollbackAsync();
+        }
+        #endregion
+
+        #region Initializer
+        private async Task<T> Initialize<T>(SqlDataReader reader) where T : new()
+        {
+            var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+
+            List<T> list = new List<T>();
+            if (await reader.ReadAsync())
+            {
+                T t = new T();
+                var parentProperties = t.GetType().GetProperties();
+                foreach (var parentProperty in parentProperties)
+                {
+
+                    if (columns.Any(s => s == parentProperty.Name))
+                        if (reader[parentProperty.Name] != DBNull.Value)
+                        {
+                            var type = parentProperty.PropertyType.IsGenericType && parentProperty.PropertyType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)) ?
+                                        Nullable.GetUnderlyingType(parentProperty.PropertyType) : parentProperty.PropertyType;
+                            var value = Convert.ChangeType(reader[parentProperty.Name], type);
+                            if (value != null)
+                                parentProperty.SetValue(t, value);
+                        }
+                }
+                return t;
+            }
+            else
+                return new T();
+        }
+        private async Task<List<T>> InitializeList<T>(SqlDataReader reader) where T : new()
+        {
+
+            var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+            List<T> list = new List<T>();
+            while (await reader.ReadAsync())
+            {
+                T t = new T();
+                var parentProperties = t.GetType().GetProperties();
+                foreach (var parentProperty in parentProperties)
+                {
+                    if (columns.Any(s => s == parentProperty.Name))
+                        if (reader[parentProperty.Name] != DBNull.Value)
+                        {
+
+                            var type = parentProperty.PropertyType.IsGenericType && parentProperty.PropertyType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)) ?
+                                        Nullable.GetUnderlyingType(parentProperty.PropertyType) : parentProperty.PropertyType;
+
+                            var value = Convert.ChangeType(reader[parentProperty.Name], type);
+                            if (value != null)
+                                parentProperty.SetValue(t, value);
+
+                        }
+
+                }
+                list.Add(t);
+            }
+            return list;
+        } 
+        #endregion
+
+        public async Task DisposeAsync()
+        {
+            await CloseConnectionAsync();
+            GC.Collect();
+        }
+    }
+}
---
+++ DataAccess/IDataAccess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IDataAccess 
+    {
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task DisposeAsync();
+        Task RollbackTransactionAsync();
+       
+        Task SqlExcuteQueryAsync(string query);
+        Task SqlExcuteQueryAsync(string query, SqlParameter[] sqlParameters);
+        Task SqlExecuteStoredProcedureAsync(SqlCommand cmd);
+        Task<T> SqlSelectAsync<T>(string query) where T : new();
+        Task<T> SqlSelectAsync<T>(string query, SqlParameter sqlParameters) where T : new();
+        Task<T> SqlSelectAsync<T>(string query, SqlParameter[] sqlParameters) where T : new();
+        Task<List<T>> SqlSelectListAsync<T>(string query) where T : new();
+        Task<List<T>> SqlSelectListAsync<T>(string query, SqlParameter sqlParameter) where T : new();
+        Task<List<T>> SqlSelectListAsync<T>(string query, SqlParameter[] sqlParameters) where T : new();
+    }
+}
